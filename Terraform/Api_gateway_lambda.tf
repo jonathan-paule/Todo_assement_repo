d01ce803e252a2f40ad42643c35ac97d40f35cf0
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 17:05:57 +0530

    Update Api_gateway_lambda.tf

--- Terraform/Api_gateway_lambda.tf
+++ Terraform/Api_gateway_lambda.tf
@@ -4,6 +4,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_apigatewayv2_api" "todo_api" {
   name          = var.api_name  #todo-http-api is the name
   protocol_type = var.protocol_type
+  auto_deploy = true
 }
 
 
