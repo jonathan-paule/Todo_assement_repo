6a6508f218367cb7eb254f0f3cf9d36fec51a9c4
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 12:37:28 +0530

    Update lambda_iam_role.tf

--- Terraform/lambda_iam_role.tf
+++ Terraform/lambda_iam_role.tf
@@ -1,3 +1,31 @@
+#lambda_role
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.project}-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# policy attachment
+resource "aws_iam_role_policy_attachment" "vpc_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+
+#inline policy
+
 resource "aws_iam_role_policy" "lambda_secrets_policy" {
   name = "lambda-secrets-policy"
   role = aws_iam_role.lambda_exec_role.id
@@ -15,4 +43,3 @@ resource "aws_iam_role_policy" "lambda_secrets_policy" {
     ]
   })
 }
-
