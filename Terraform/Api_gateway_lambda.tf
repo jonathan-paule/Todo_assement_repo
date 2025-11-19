736c308a6d3b2aeb4525f150e208688838fa8e91
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 17:10:40 +0530

    Update Api_gateway_lambda.tf

--- Terraform/Api_gateway_lambda.tf
+++ Terraform/Api_gateway_lambda.tf
@@ -4,7 +4,6 @@ data "aws_caller_identity" "current" {}
 resource "aws_apigatewayv2_api" "todo_api" {
   name          = var.api_name  #todo-http-api is the name
   protocol_type = var.protocol_type
-  auto_deploy = true
 }
 
 
@@ -13,6 +12,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.todo_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.todo_lambda.invoke_arn
+ 
 }
 
 resource "aws_apigatewayv2_route" "default_route" {
@@ -32,4 +32,10 @@ resource "aws_lambda_permission" "apigw_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.todo_lambda.function_name
   principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.todo_api.id
+  name        = "$default"
+  auto_deploy = true
 }
