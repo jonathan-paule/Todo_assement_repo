f307fbbc2bcd9dd1012166872cf6e9f5e84d359a
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 12:22:44 +0530

    Update lambda_iam_role.tf

--- Terraform/lambda_iam_role.tf
+++ Terraform/lambda_iam_role.tf
@@ -1,1 +1,18 @@
+resource "aws_iam_role_policy" "lambda_secrets_policy" {
+  name = "lambda-secrets-policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:todoapp/db_credentials*"
+      }
+    ]
+  })
+}
 
