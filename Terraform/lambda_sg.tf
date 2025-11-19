103f3ba6f1a274bed687b6f48d5a13d5b243fbcb
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 00:10:31 +0530

    Create lambda_sg.tf

---
+++ Terraform/lambda_sg.tf
@@ -0,0 +1,16 @@
+#lambda sg
+resource "aws_security_group" "lambda_sg" {
+  name        = "${var.project}-lambda-sg"
+  description = "Lambda SG"
+  vpc_id      = aws_vpc.main.id
+
+  # No inbound for Lambda
+
+  # Outbound open 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
