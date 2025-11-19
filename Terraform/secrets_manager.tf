f47643fb337b753bcd8cb0efc5c10d1b85390893
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 12:50:15 +0530

    Create secrets_manager.tf

---
+++ Terraform/secrets_manager.tf
@@ -0,0 +1,14 @@
+resource "aws_secretsmanager_secret" "db" {
+  name = "todoapp/db_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "db" {
+  secret_id     = aws_secretsmanager_secret.db.id
+  secret_string = jsonencode({
+    username = var.db_username
+    password = var.db_password
+    host     = aws_db_instance.todo_rds.address
+    dbname   = var.db_name
+    port     = "5432"
+  })
+}
