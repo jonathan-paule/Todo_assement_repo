7251c5d52ab15c66c6745bddad8a08df745f84c2
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 00:04:38 +0530

    Update security_groups.tf

--- Terraform/security_groups.tf
+++ Terraform/security_groups.tf
@@ -21,29 +21,36 @@ resource "aws_security_group" "rds_sg" {
   name        = "${var.project}-rds-sg"
   description = "RDS Security Group"
   vpc_id      = aws_vpc.main.id
+}
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda_sg.id]
-    description     = "Allow Lambda"
-  }
+# Allow Lambda to connect to RDS
+resource "aws_security_group_rule" "rds_from_lambda" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.lambda_sg.id
+}
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-    description     = "Allow Bastion"
-  }
+# Allow Bastion to connect to RDS
+resource "aws_security_group_rule" "rds_from_bastion" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.bastion_sg.id
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Outbound
+resource "aws_security_group_rule" "rds_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # bastion sg
