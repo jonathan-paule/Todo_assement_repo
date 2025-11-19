c1597bf6cf11f7a0abb9f804df4cb1cdc66681f6
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 12:20:38 +0530

    Update bastion.tf

--- Terraform/bastion.tf
+++ Terraform/bastion.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami           = var.ami_id  # Amazon Linux 2
   instance_type = var.instance_type
 
-  subnet_id              = aws_subnet.public_a.id
+  subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   associate_public_ip_address = true
