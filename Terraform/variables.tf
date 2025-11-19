60ca005f8a59238ccbca9900b383a7fe8dc5b716
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 12:10:01 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,71 @@
+variable "ami_id" {
+    description = "AMI id for your instance"
+    type = string
+    default = "ami-0c02fb55956c7d316"
+}
+variable "instance_type" {
+    description = "ec2 instance type"
+    type = string
+    default = "t2.micro"
+  
+}
+
+variable "key_name" {
+    description = "key name"
+    type = string
+    default = "todo_key_pair"
+  
+}
+variable "project" {
+    description = "project name"
+    type = string
+    default = "Todo_api"
+
+  
+}
+
+variable "function_name" {
+    description = "lambda_fun_name"
+    type = string
+    default = "todo-crud"
+}
+variable "runtime" {
+    description = "lambda runtime" 
+    type = string
+    default = "python3.12"
+  
+}
+
+variable "filename" {
+    description = "file name for lambda" 
+    type = string
+    default = "lambda/todo-api.zip"
+  
+}
+variable "engine" {
+    description = "RDS engine" 
+    type = string
+    default = "postgres"
+  
+}
+
+variable "instance_class" {
+    description = "instance class for RDS" 
+    type = string
+    default = "db.t3.micro"
+  
+}
+
+variable "api_name" {
+    description = "api_name" 
+    type = string
+    default = "todo-http-api"
+  
+}
+
+variable "protocol_type" {
+    description = " protocal_type"
+    type = string
+    default = "HTTP"
+  
+}
