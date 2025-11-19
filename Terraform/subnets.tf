caa4651d6600786478143fc174d42ddd90a74d0f
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-19 15:16:30 +0530

    Update subnets.tf

--- Terraform/subnets.tf
+++ Terraform/subnets.tf
@@ -7,4 +7,13 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+}
+
+
+resource "aws_subnet" "private_subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
 }
