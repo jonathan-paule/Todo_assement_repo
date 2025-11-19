c9320a88d01450c6334c7953b01509eb3d485f7d
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 00:25:23 +0530

    Update lambda_sg.tf

--- Terraform/lambda_sg.tf
+++ Terraform/lambda_sg.tf
@@ -1,16 +1,18 @@
-#lambda sg
 resource "aws_security_group" "lambda_sg" {
   name        = "${var.project}-lambda-sg"
   description = "Lambda SG"
   vpc_id      = aws_vpc.main.id
 
-  # No inbound for Lambda
-
-  # Outbound open 
+  # Lambda does not require inbound rules
+  # Allow all outbound so Lambda can reach Secrets Manager, RDS DNS, etc.
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project}-lambda-sg"
   }
 }
