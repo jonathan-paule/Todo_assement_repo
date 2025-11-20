9c06c753a3ba059340a2948d2960b352f0b63d6f
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 15:16:02 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -88,6 +88,8 @@ variable "region" {
 variable "db_username" {
   description = "Database master username"
   type        = string
+  sensitive   = true
+
 }
 
 variable "db_password" {
@@ -99,6 +101,7 @@ variable "db_password" {
 variable "db_name" {
   description = "Database name"
   type        = string
+  sensitive   = true
 }
 
 
