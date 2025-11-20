d79c2c87a5e48127486e13702ea17c71939fe7b4
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 17:03:01 +0530

    Update rds_security_group.tf

--- Terraform/rds_security_group.tf
+++ Terraform/rds_security_group.tf
@@ -18,6 +18,15 @@ resource "aws_security_group_rule" "rds_from_lambda" {
   source_security_group_id = aws_security_group.lambda_sg.id
   description              = "Allow Lambda to connect to RDS (all ports)"
 }
+resource "aws_security_group_rule" "rds_from_bastion" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.bastion_sg.id
+  description              = "Allow Bastion to connect to MySQL"
+}
 
 
 
