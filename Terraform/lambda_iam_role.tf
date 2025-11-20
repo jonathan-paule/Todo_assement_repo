8b5466540bee3132a922da2342af26fb3a13b061
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 23:43:09 +0530

    Update lambda_iam_role.tf

--- Terraform/lambda_iam_role.tf
+++ Terraform/lambda_iam_role.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-# Attach AWS-managed policy for VPC access (only needed if Lambda is in a VPC)
+
 resource "aws_iam_role_policy_attachment" "vpc_access" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
