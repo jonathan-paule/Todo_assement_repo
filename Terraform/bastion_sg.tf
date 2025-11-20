13358c9af630e6e9cf6523affcb525bfe4e3e6ce
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-20 21:06:59 +0530

    Update bastion_sg.tf

--- Terraform/bastion_sg.tf
+++ Terraform/bastion_sg.tf
@@ -10,11 +10,19 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "tcp"
     cidr_blocks = [var.my_ip]   # "your-ip/32"
   }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "egress_mysql" {
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  
+  # CORRECTED: The attribute to reference the ID is simply '.id'
+  source_security_group_id = aws_security_group.rdsmysql_sg.id 
+  
+  # This correctly references the ID of the Bastion SG defined above
+  security_group_id        = aws_security_group.bastion_sg.id 
+
+  description              = "to access db
 }
